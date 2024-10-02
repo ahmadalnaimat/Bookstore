5eb1b3133d70d016d1f044508df8a3bb5befccf0
Ahmad Alfaza'at <23039673@student.ltuc.com>
2024-10-02 23:22:31 +0300

    done

---
+++ Bookstore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =10)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =2)]
+        public string Author { get; set; }
+        [Required]
+        [Range(0, 1000.0)]
+        public decimal Price { get; set; }
+        [Required]
+        public string Genre { get; set; }
+    }
+}
